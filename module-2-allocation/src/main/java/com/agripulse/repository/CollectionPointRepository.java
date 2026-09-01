d00822eb1ae2305d522b0a45e06888d8d39e6f07
NIGEETH MALEESHA <91341553+nigeethmaleesha@users.noreply.github.com>
2026-09-02 00:31:48 +0530

    backend run error fix

--- module-2-allocation/src/main/java/com/agripulse/repository/CollectionPointRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agripulse.repository;
-
-import com.agripulse.model.entity.CollectionPoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CollectionPointRepository extends JpaRepository<CollectionPoint, String> {
-}
